2700f4e3e9a2646f0a990aee8da8ded30c81c5b2
Pablo Franco <pabloafranco@hotmail.com>
2021-08-27 19:06:05 -0300

    cambie base

--- main.py
+++ main.py
@@ -2,13 +2,31 @@ import pymysql
 # Tengo que instalar esto pip install python-decouple 
 from decouple import config
 
+USERS_TABLE = """CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT  PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email  VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+"""
+
 if __name__  == '__main__':
     
-    connect = pymysql.Connect(host='localhost', 
+    try:
+        print (config('DB_MYSQL'))
+        connect = pymysql.Connect(host='localhost', 
                                   port=3306, 
                                   user=config('USER_MYSQL'), 
                                   passwd=config('PASWORD_MYSQL'), 
                                   db=config('DB_MYSQL'))
+        with connect.cursor() as cursor:
+
+            cursor.execute(USERS_TABLE)
+
+    except pymysql.err.OperationalError as err:
+        print ('No fue posible hacer la conexion')
+        print (err)
 
 
 """"
